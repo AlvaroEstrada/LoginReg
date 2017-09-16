65ac811ecd18b26295ba61cf01dd89039f4e692f
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-09-16 15:32:14 +0200

    Commit

---
+++ app/src/main/java/com/alvaropedrajas/loginreg/Controlador.java
@@ -0,0 +1,65 @@
+package com.alvaropedrajas.loginreg;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class Controlador implements View.OnClickListener{
+
+    final String usuario = "usuario";
+    final String password = "prueba";
+    EditText etUser, etPass;
+    boolean loginFlag;
+    String getUsuario, getPassword;
+
+    private Activity activity;
+
+
+    public Controlador (Activity activity){
+        this.activity = activity;
+        Button btn = (Button) activity.findViewById(R.id.btnJoin);
+        btn.setOnClickListener(this);
+
+    }
+
+    public void login(View v) {
+        etUser = (EditText) activity.findViewById(R.id.etUser);
+        etPass = (EditText) activity.findViewById(R.id.etPass);
+
+        getUsuario = etUser.getText().toString();
+        getPassword = etPass.getText().toString();
+
+        String empty = "";
+
+        if (!Objects.equals(getUsuario, empty) && !Objects.equals(getPassword, empty)){
+            if (Objects.equals(getUsuario, usuario) && Objects.equals(getPassword, password)){
+                loginFlag = true;
+            }else{
+                Toast.makeText(v.getContext(), "¡Usuario o contraseña erroneo!", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(v.getContext(), "¡Se necesita usuario y contraseña!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        login(v);
+
+        if(MainActivity.this){
+
+        }
+
+        if (loginFlag){
+            Intent intent = new Intent(activity, UserdataActivity.class);
+            activity.startActivityForResult(intent, 0);
+        }
+    }
+}
--- app/src/main/java/com/alvaropedrajas/loginreg/MainActivity.java
+++ app/src/main/java/com/alvaropedrajas/loginreg/MainActivity.java
@@ -1,65 +1,23 @@
 package com.alvaropedrajas.loginreg;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.Objects;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    final String usuario = "usuario";
-    final String password = "prueba";
-    EditText etUser, etPass;
-    boolean loginFlag;
-
-    String getUsuario, getPassword;
-
+    private Controlador controlador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.btnJoin);
-        btn.setOnClickListener(this);
+        controlador = new Controlador(this);
 
-    }
 
-    public void login(View v) {
-        etUser = (EditText) this.findViewById(R.id.etUser);
-        etPass = (EditText) this.findViewById(R.id.etPass);
-
-        getUsuario = etUser.getText().toString();
-        getPassword = etPass.getText().toString();
-
-        String empty = "";
-
-        if (!Objects.equals(getUsuario, empty) && !Objects.equals(getPassword, empty)){
-            if (Objects.equals(getUsuario, usuario) && Objects.equals(getPassword, password)){
-                loginFlag = true;
-            }else{
-                Toast.makeText(v.getContext(), "¡Usuario o contraseña erroneo!", Toast.LENGTH_SHORT).show();
-            }
-        }else{
-            Toast.makeText(v.getContext(), "¡Se necesita usuario y contraseña!", Toast.LENGTH_SHORT).show();
-        }
     }
 
 
 
-    @Override
-    public void onClick(View v) {
 
-        login(v);
-
-        if (loginFlag){
-            Intent intent = new Intent(this, UserdataActivity.class);
-            startActivityForResult(intent, 0);
-        }
-    }
 }
